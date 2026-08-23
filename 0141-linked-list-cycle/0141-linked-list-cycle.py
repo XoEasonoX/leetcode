886bfa1663f6349e13192cefb2bca651d125d6cf
OoEasonoO <yizhongchen961206@gmail.com>
2026-08-23 23:50:49 +0800

    Time: 35 ms (68.97%), Space: 19.4 MB (65.97%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def hasCycle(self, head):
+        fast = head
+        slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                 return True          
+        return False
