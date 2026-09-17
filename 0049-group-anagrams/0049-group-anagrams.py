dc49dca10f565d1a31328cfa064331aa5463b2fd
OoEasonoO <yizhongchen961206@gmail.com>
2026-09-17 22:58:53 +0800

    Time: 24 ms (35.27%), Space: 16 MB (71.79%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        g = {}
+        for s in strs:
+            key = ''.join(sorted(s))
+            if key not in g :
+                g[key] = [] 
+            g[key].append(s)
+        return list(g.values())
